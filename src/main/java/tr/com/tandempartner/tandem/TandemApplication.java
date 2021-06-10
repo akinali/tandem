c2ee8b41f86ed86784c61c55b2bd9190bd519dec
Ali Akin <akin.ali@hotmail.com>
2021-06-10 21:09:13 +0300

    swagger 2 added for rest apis documentation

--- src/main/java/tr/com/tandempartner/tandem/TandemApplication.java
+++ src/main/java/tr/com/tandempartner/tandem/TandemApplication.java
@@ -2,10 +2,15 @@ package tr.com.tandempartner.tandem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+ 	 	
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import tr.com.tandempartner.tandem.entity.user.User;
-
-
+@EnableSwagger2
 @SpringBootApplication
 public class TandemApplication {
 
@@ -16,5 +21,13 @@ public class TandemApplication {
 		
 		
 	}
+	
+	@Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.basePackage("tr.com.tandempartner.tandem"))                                     
+          .build();                                           
+    }
 
 }
